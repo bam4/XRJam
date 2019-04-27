5482d60af14b7a29e2dae05a48b6ff211edde12e
Brett Moody <brettmoody@me.com>
2019-04-27 17:37:03 -0400

    Added a new dodgeball mechanic (currently broken, not working.)

--- XRJam/Assets/InterrogateSuspect.cs
+++ XRJam/Assets/InterrogateSuspect.cs
@@ -7,12 +7,18 @@ public class InterrogateSuspect : MonoBehaviour {
 
 	public bool IsSecretAgent = false;
 	public GameObject explosion;
+	public AudioSource myAudioSource;
+	public AudioClip Annoyed;
 
 	
 	public void Interrogate() {
 		if (IsSecretAgent) {
 			Instantiate(explosion, this.transform.position, Quaternion.identity);
 			Destroy(this.gameObject);
+		} else {
+			myAudioSource.clip = Annoyed;
+			myAudioSource.Play();
+
 		}
 	}
 }
---
+++ XRJam/Assets/ThrowDodgeball.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ThrowDodgeball : MonoBehaviour {
+
+	public GameObject Dodgeball;
+	
+
+	public Valve.VR.InteractionSystem.Hand hand;
+	
+	bool DodgeballReady = true;
+
+	// Update is called once per frame
+	void Update () {
+		if (SteamVR_Actions._default.InteractUI.GetState(SteamVR_Input_Sources.Any) ) {
+			if (DodgeballReady) {
+				SpawnDodgeball();
+				DodgeballReady = false;
+			}
+		}
+	}
+
+	
+
+	public void SpawnDodgeball () {
+		Debug.Log("Spawning dodgeball");
+		GameObject spawnedDodgeball = GameObject.Instantiate( Dodgeball, hand.transform.position, Quaternion.identity );
+		spawnedDodgeball.SetActive( true );
+		
+		hand.AttachObject( spawnedDodgeball, Valve.VR.InteractionSystem.GrabTypes.None, Valve.VR.InteractionSystem.Hand.AttachmentFlags.SnapOnAttach );
+
+	}
+
+	
+}
